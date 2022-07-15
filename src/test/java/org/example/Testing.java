8c3ea36955bc4d627e25f5b9f87f7f00a27f2b55
Akatofff <super.kostya.a@yandex.ru>
2022-07-15 17:10:57 +0300

    Fully working Test.

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -4,15 +4,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Testing {
     public final String DRIVER_PATH = "C:/Users/Акатов/Desktop/Testing/chromedriver.exe";
     public final String DRIVER_TYPE = "webdriver.chrome.driver";
@@ -20,54 +22,69 @@ public class Testing {
     public String BASE_URL = "https://travelline.ru/";
     public final String LINKS_ATTRIBUTE = "href";
     public final String LINKS_TAG = "a";
-    int i = 1;
-@Before
+    Set<String> allLinks = new HashSet<>();
+    FileWriter broken = new FileWriter("broken.txt");
+    FileWriter working = new FileWriter("working.txt");
 
-    public void driverSetup(){
+    public Testing() throws IOException {
+    }
+
+    @Before
+
+    public void driverSetup() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications","--ignore-certificate-errors","--disable-extensions");
-        System.setProperty(DRIVER_TYPE,DRIVER_PATH);
+        options.addArguments("--disable-notifications", "--ignore-certificate-errors", "--disable-extensions");
+        System.setProperty(DRIVER_TYPE, DRIVER_PATH);
         driver = new ChromeDriver(options);
-//        driver.manage().window().maximize();
-        driver.get(BASE_URL);
     }
 
     @org.junit.Test
 
-    public void BrokenLinks() throws Exception{
+    public void testMethod() {
+        brokenLinks(BASE_URL);
+    }
 
-        List<WebElement> allLinks = driver.findElements(By.tagName(LINKS_TAG));
-        for(WebElement link:allLinks) {
+    public void brokenLinks(String rootUrl) {
+        driver.get(rootUrl);
+        var currentPageLinks = driver.findElements(By.tagName(LINKS_TAG))
+                .stream().map(e -> e.getAttribute(LINKS_ATTRIBUTE)).collect(Collectors.toSet());
+        currentPageLinks.removeAll(allLinks);
+        allLinks.addAll(currentPageLinks);
+        for (var link : currentPageLinks) {
             try {
-                String urlLink = link.getAttribute(LINKS_ATTRIBUTE);
-                URL url = new URL(urlLink);
+                URL url = new URL(link);
                 HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
                 httpURLConnect.setConnectTimeout(5000);
                 httpURLConnect.connect();
-                if (httpURLConnect.getResponseCode()>=400){
-//                if ((httpURLConnect.getResponseCode()>200 || httpURLConnect.getResponseCode()<200) && (httpURLConnect.getResponseCode()>302 || httpURLConnect.getResponseCode()<302)) {
-                        try (FileWriter broken = new FileWriter("broken.txt")) {
-                            broken.write("Broken Link: " + urlLink);
+                if (link.contains("travelline.ru")) {
+                    if (httpURLConnect.getResponseCode() >= 400) {
+                        try {
+                            broken.write("Broken Link: " + link + " | Response: " + httpURLConnect.getResponseMessage() + "\n");
+                            broken.flush();
                         } catch (Exception e) {
                         }
-                        System.out.println(urlLink + " - " + httpURLConnect.getResponseMessage() + " is a broken link");
+                        System.out.println(link + " - " + httpURLConnect.getResponseMessage() + " - is a broken link");
                     } else {
-                        try (PrintWriter working = new PrintWriter("working.txt")) {
-                            working.println("Working link:" + "\n" + urlLink + "\n");
-                            working.append(' ');
-                            working.close();
-                            System.out.println(urlLink + " - " + httpURLConnect.getResponseMessage());
-                        }catch (Exception e){ }
+                        brokenLinks(link);
+                        try {
+                            working.write("Working link: " + link + " | Response: " + httpURLConnect.getResponseMessage() + "\n");
+                            working.flush();
+                            System.out.println(link + " - " + httpURLConnect.getResponseMessage());
+                        } catch (Exception e) {
+                        }
                     }
+                }
 
-                }catch (Exception e) { }
-
+            } catch (Exception e) {
             }
         }
-
-    @After
-    public void closeDriver(){
-        driver.close();
     }
 
+    @After
+
+    public void closeDriver() throws IOException {
+        driver.close();
+        working.close();
+        broken.close();
+    }
 }
